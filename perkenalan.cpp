3272cf17f6025ee45480e22fc6dd7491f2f18e9d
Andhika Bagus <andhika2100018232@gmail.com>
2022-03-16 22:03:58 +0700

    commit

--- perkenalan.cpp
+++ perkenalan.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
 	cout<<"Nama  : Andhika Bagus"<<endl;
 	cout<<"Kelas : E"<<endl<<endl;
-	cout<<"UPDATE"<<endl;
+	cout<<"UPDATE 0.0.1"<<endl;
+	cout<<"Testing"<<endl;
 }
