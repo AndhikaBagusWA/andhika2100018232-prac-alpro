a6f23dce64072fcd963c0eaf90a7b1809a0dcdce
Andhika Bagus <andhika2100018232@gmail.com>
2022-03-17 11:28:21 +0700

    Update 0.0.2

--- perkenalan.cpp
+++ perkenalan.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	cout<<"Nama  : Andhika Bagus"<<endl;
 	cout<<"Kelas : E"<<endl<<endl;
-	cout<<"UPDATE 0.0.1"<<endl;
-	cout<<"Testing"<<endl;
-	cout<<"Hello world"<<endl;
+	cout<<"UPDATE 0.0.2"<<endl;
+	cout<<"Tes 1"<<endl;
+	cout<<"Tes 2"<<endl;
 }
